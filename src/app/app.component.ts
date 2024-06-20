d0a86a27c92f7ec3c9d3cf2c253fac0c9d6108de
Gregory Pereverzev <boot762@gmail.com>
2024-06-20 10:33:32 +0300

    feat: add fonts

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,10 +2,12 @@ import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterOutlet } from '@angular/router';
 
+import { ProfileCardComponent } from './common-ui/profile-card/profile-card.component';
+
 @Component({
   selector: 'tt-root',
   standalone: true,
-  imports: [CommonModule, RouterOutlet],
+  imports: [CommonModule, RouterOutlet, ProfileCardComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss',
 })
