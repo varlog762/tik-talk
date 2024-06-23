f6a3a31b8059b1916a7fd280d993a3ebcf661a5a
Gregory Pereverzev <boot762@gmail.com>
2024-06-23 15:33:36 +0300

    feat: add AuthService

--- src/app/pages/login-page/login-page.component.ts
+++ src/app/pages/login-page/login-page.component.ts
@@ -1,10 +1,30 @@
-import { Component } from '@angular/core';
+import { Component, OnInit, inject } from '@angular/core';
+import { FormBuilder, FormGroup, ReactiveFormsModule } from '@angular/forms';
 
 @Component({
   selector: 'tt-login-page',
   standalone: true,
-  imports: [],
+  imports: [ReactiveFormsModule],
   templateUrl: './login-page.component.html',
   styleUrl: './login-page.component.scss',
 })
-export class LoginPageComponent {}
+export class LoginPageComponent implements OnInit {
+  fb = inject(FormBuilder);
+
+  form!: FormGroup;
+
+  ngOnInit(): void {
+    this.initializeForm();
+  }
+
+  initializeForm(): void {
+    this.form = this.fb.group({
+      username: '',
+      password: '',
+    });
+  }
+
+  onSubmit(): void {
+    console.dir(this.form.value);
+  }
+}
