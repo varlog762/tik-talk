9a04b002177c3e1a56a066b1d8afd09a18cb97eb
Gregory Pereverzev <boot762@gmail.com>
2024-06-23 15:43:32 +0300

    feat: add LoginResponseInterface

--- src/app/auth/services/auth.service.ts
+++ src/app/auth/services/auth.service.ts
@@ -1,9 +1,14 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable, inject } from '@angular/core';
+import { Observable } from 'rxjs/internal/Observable';
+
+import { LoginRequestInterface } from '../../models/login-request.interface';
 
 @Injectable({
   providedIn: 'root',
 })
 export class AuthService {
   http = inject(HttpClient);
+
+  login(payload: LoginRequestInterface): Observable<> {}
 }
